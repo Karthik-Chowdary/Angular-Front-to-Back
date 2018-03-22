be8bd0e33583597b39d929a8c5ed6daf8787ec52
Karthik-Chowdary <svskarthikchowdary@gmail.com>
2018-03-22 02:48:42 -0400

    Properties, Methods and String Interpolation

--- angularapp/src/app/components/user/user.component.ts
+++ angularapp/src/app/components/user/user.component.ts
@@ -7,6 +7,30 @@ import { Component } from "@angular/core"
 })
 
 export class UserComponent{
+    //Properties
+    firstName = 'John';
+    lastName = 'Doe';
+    age = 30;
+    address = {
+        street : '730 Riddle Rd',
+        city : 'Cincinnati',
+        state : 'Ohio'
+    };
 
+    //Methods
+    constructor(){
+        this.sayHello();
+        console.log(this.age);
+        this.hasBirthday();
+        console.log(this.age);
+    }
+
+    sayHello(){
+        console.log(`Hello ${this.firstName}`);
+    }
+
+    hasBirthday(){
+        this.age += 1;
+    }
 }
 

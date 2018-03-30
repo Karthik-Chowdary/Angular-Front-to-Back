0310870fa2c37201cd216b54c8b50f0f87d44240
Karthik-Chowdary <svskarthikchowdary@gmail.com>
2018-03-29 23:36:32 -0400

    login edits

--- angularapp/src/app/components/login/login.component.ts
+++ angularapp/src/app/components/login/login.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css',
+'../../node_modules/bootstrap/dist/css/bootstrap.css']
 })
 export class LoginComponent implements OnInit {
 

8622b64ba90a8c3b926315dcad1cf0ba007c7318
Karthik-Chowdary <svskarthikchowdary@gmail.com>
2018-03-31 23:52:45 -0400

    Login-Component-CSS, TS revamp

--- angularapp/src/app/components/login/login.component.ts
+++ angularapp/src/app/components/login/login.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { Router } from '@angular/router';
+import { moveIn } from '../router.animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css',
-'../../node_modules/bootstrap/dist/css/bootstrap.css']
+  styleUrls: ['./login.component.css'],
+  animations: [moveIn()],
+  host: {'[@moveIn]': ''}
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  error: any;
+  constructor(public af: AngularFire,private router: Router) {
+
+      this.af.auth.subscribe(auth => { 
+      if(auth) {
+        this.router.navigateByUrl('/members');
+      }
+    });
+
+  }
+
+  loginFb() {
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup,
+    }).then(
+        (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+        (err) => {
+        this.error = err;
+      })
+  }
+
+  loginGoogle() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    }).then(
+        (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+        (err) => {
+        this.error = err;
+      })
+  }
+
 
   ngOnInit() {
   }

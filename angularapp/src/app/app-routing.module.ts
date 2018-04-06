be346d18d17b7e57dc49556dcb0d759046c01bce
Karthik-Chowdary <svskarthikchowdary@gmail.com>
2018-04-05 23:22:04 -0400

    Creating and Mapping Routes

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component'
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'posts', component: PostsComponent}
+];
 @NgModule({
   exports: [RouterModule],
   imports: [
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -12,7 +12,8 @@ import { UserService } from './services/User.service';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostService } from './services/post.service';
 import { PostFormComponent } from './components/post-form/post-form.component';
-import { AppRoutingModule } from './/app-routing.module'
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './components/home/home.component'
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { AppRoutingModule } from './/app-routing.module'
     UsersComponent,
     NavbarComponent,
     PostsComponent,
-    PostFormComponent
+    PostFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

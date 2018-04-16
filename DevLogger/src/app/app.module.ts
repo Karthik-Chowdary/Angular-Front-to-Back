3237356e42b8b59c9a9fdc36dacd761918f80f8f
Karthik-Chowdary <svskarthikchowdary@gmail.com>
2018-04-15 23:59:21 -0400

    Comp

--- DevLogger/src/app/app.module.ts
+++ DevLogger/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { LogService } from './services/log.service';
     LogsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [LogService],
   bootstrap: [AppComponent]
--- DevLogger/src/app/components/log-form/log-form.component.ts
+++ DevLogger/src/app/components/log-form/log-form.component.ts
@@ -8,12 +8,19 @@ import { LogService } from '../../services/log.service'
   styleUrls: ['./log-form.component.css']
 })
 export class LogFormComponent implements OnInit {
+  id: string;
+  text: string;
+  date: any;
 
   constructor(private logService: LogService) { }
 
   ngOnInit() {
     this.logService.selectedLog.subscribe(log => {
-      console.log(log);
+      if(log.id !== null){
+        this.id = log.id;
+        this.text = log.text;
+        this.date = log.date;
+      }
     });
   }
 
